c7a4b593801652a48cd296f259055d5f232b54a2
Suhail Sharieff <suhailsharieffsharieff@gmail.com>
2025-04-25 00:01:34 +0530

    setup done

--- server/Utils/_04_Api_Error.utils.js
+++ server/Utils/_04_Api_Error.utils.js
@@ -1,13 +1,24 @@
-class ApiError extends Error{
-    constructor(statusCode,message,errors,stack) {
+class ApiError extends Error {
+    constructor(
+        statusCode,
+        message= "API Error",
+        errors = [],
+        stack = ""
+    ){
         super(message)
-        this.statusCode=statusCode,
-        this.message=message,
-        this.data=null,
-        this.success=false,
-        this.errors=errors
+        this.statusCode = statusCode
+        this.data = null
+        this.message = message
+        this.success = false;
+        this.errors = errors
+
+        if (stack) {
+            this.stack = stack
+        } else{
+            Error.captureStackTrace(this, this.constructor)
+        }
+
     }
 }
-
 
 export {ApiError}
