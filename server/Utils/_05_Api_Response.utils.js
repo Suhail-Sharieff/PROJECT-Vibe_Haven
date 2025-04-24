581ab156f3aaf1d0d79869a6ce930dfa62244358
Suhail Sharieff <suhailsharieffsharieff@gmail.com>
2025-04-24 23:53:35 +0530

    setup

--- server/Utils/_05_Api_Response.utils.js
+++ server/Utils/_05_Api_Response.utils.js
@@ -1,10 +1,13 @@
-class ApiResponse {
-    constructor(statusCode, data, message = "Success"){
-        this.statusCode = statusCode
-        this.data = data
-        this.message = message
-        this.success = statusCode < 400
+class ApiError extends Error{
+    constructor(statusCode,message,errors,stack) {
+        super(message)
+        this.statusCode=statusCode,
+        this.message=message,
+        this.data=null,
+        this.success=false,
+        this.errors=errors
     }
 }
 
-export { ApiResponse }
+
+export {ApiError}
--- server/models/_01_user.model.js
+++ server/models/_01_user.model.js
@@ -1,96 +1,45 @@
-import mongoose, {Schema} from "mongoose";
-import jwt from "jsonwebtoken"
-import bcrypt from "bcrypt"
+import mongoose from "mongoose";
 
-const userSchema = new Schema(
+
+const userSchema = new mongoose.Schema(
     {
-        username: {
-            type: String,
-            required: true,
-            unique: true,
-            lowercase: true,
-            trim: true, 
-            index: true
+        user_name:{
+            type:String,
+            unique:true,
+            required:true,
+            trim:true,
+            index:true,
+        }, 
+        email:{
+            type:String,
+            unique:true,
+            required:true,
+            trim:true,
         },
-        email: {
-            type: String,
-            required: true,
-            unique: true,
-            lowecase: true,
-            trim: true, 
+        password:{
+            type:String,
+            required:true,
         },
-        fullName: {
-            type: String,
-            required: true,
-            trim: true, 
-            index: true
+        avatar_url:{
+            type:String,
+            required:true,
+            default:"https://www.gravatar.com/avatar/"
         },
-        avatar: {
-            type: String, // cloudinary url
-            required: true,
-        },
-        coverImage: {
-            type: String, // cloudinary url
-        },
-        watchHistory: [
+        some_array:[
             {
-                type: Schema.Types.ObjectId,
-                ref: "Video"
+                type:mongoose.Schema.Types.ObjectId,
+                ref:"SubTodo"
             }
         ],
-        password: {
-            type: String,
-            required: [true, 'Password is required']
-        },
-        refreshToken: {
-            type: String
-        }
+        someStatus:{
+            type:String,
+            enum:['PENDING','COMPLETED','CANCELLED'],
+            default:'PENDING'
+       },
+       refresh_token:{
+        type:String
+       }
+    },{timestamps:true}
+);
 
-    },
-    {
-        timestamps: true
-    }
-)
-//pre matlab, koi bhi data ko save karne se pehle ye function run karlo
-userSchema.pre("save", async function (next) {
-    if(!this.isModified("password")) return next();//save karne se pehle deklo if password is same as bfr or not
-
-    this.password = await bcrypt.hash(this.password, 10);//password change hua he, to hash kar do aur next movve karo for abt 10 rounds of security
-    next();
-})
-
-
-//we can create our own utility methods, syntax: userSchema.methods.methodName = function(){}
-
-userSchema.methods.isPasswordCorrect = async function(password){
-    return await bcrypt.compare(password, this.password)
-}
-
-userSchema.methods.generateAccessToken = function(){
-    return jwt.sign(
-        {
-            _id: this._id,
-            email: this.email,
-            username: this.username,
-            fullName: this.fullName
-        },
-        process.env.ACCESS_TOKEN_SECRET,
-        {
-            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
-        }
-    )
-}
-userSchema.methods.generateRefreshToken = function(){
-    return jwt.sign(
-        {
-            _id: this._id,
-            
-        },
-        process.env.REFRESH_TOKEN_SECRET,
-        {
-            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
-        }
-    )
-}
-
-export const User = mongoose.model("User", userSchema)
+export const User=mongoose.model("User",userSchema);
