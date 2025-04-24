a1847b775f4c5ebbf266c49103604f9848c4075d
Suhail Sharieff <suhailsharieffsharieff@gmail.com>
2025-04-24 23:11:05 +0530

    jwt stuff added

--- server/models/_01_user.model.js
+++ server/models/_01_user.model.js
@@ -1,45 +1,96 @@
-import mongoose from "mongoose";
+import mongoose, {Schema} from "mongoose";
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
 
-
-const userSchema = new mongoose.Schema(
+const userSchema = new Schema(
     {
-        user_name:{
-            type:String,
-            unique:true,
-            required:true,
-            trim:true,
-            index:true,
-        }, 
-        email:{
-            type:String,
-            unique:true,
-            required:true,
-            trim:true,
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true, 
+            index: true
         },
-        password:{
-            type:String,
-            required:true,
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowecase: true,
+            trim: true, 
         },
-        avatar_url:{
-            type:String,
-            required:true,
-            default:"https://www.gravatar.com/avatar/"
+        fullName: {
+            type: String,
+            required: true,
+            trim: true, 
+            index: true
         },
-        some_array:[
+        avatar: {
+            type: String, // cloudinary url
+            required: true,
+        },
+        coverImage: {
+            type: String, // cloudinary url
+        },
+        watchHistory: [
             {
-                type:mongoose.Schema.Types.ObjectId,
-                ref:"SubTodo"
+                type: Schema.Types.ObjectId,
+                ref: "Video"
             }
         ],
-        someStatus:{
-            type:String,
-            enum:['PENDING','COMPLETED','CANCELLED'],
-            default:'PENDING'
-       },
-       refresh_token:{
-        type:String
-       }
-    },{timestamps:true}
-);
+        password: {
+            type: String,
+            required: [true, 'Password is required']
+        },
+        refreshToken: {
+            type: String
+        }
 
-export const User=mongoose.model("User",userSchema);
+    },
+    {
+        timestamps: true
+    }
+)
+//pre matlab, koi bhi data ko save karne se pehle ye function run karlo
+userSchema.pre("save", async function (next) {
+    if(!this.isModified("password")) return next();//save karne se pehle deklo if password is same as bfr or not
+
+    this.password = await bcrypt.hash(this.password, 10);//password change hua he, to hash kar do aur next movve karo for abt 10 rounds of security
+    next();
+})
+
+
+//we can create our own utility methods, syntax: userSchema.methods.methodName = function(){}
+
+userSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullName: this.fullName
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
+export const User = mongoose.model("User", userSchema)
