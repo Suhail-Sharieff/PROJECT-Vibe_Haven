0f68f008802464beee71f99f0c83198cb970db00
Suhail Sharieff <suhailsharieffsharieff@gmail.com>
2025-05-08 23:59:29 +0530

    mongo aggregation pipelines learnt

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,11 @@
+// MongoDB Playground
+// Use Ctrl+Space inside a snippet or a string literal to trigger completions.
+
+// The current database to use.
+use("test");
+
+// Find a document in a collection.
+const x=db.getCollection("users")
+
+
+// x.findOne()
